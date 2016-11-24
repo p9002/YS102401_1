3eb58cbc7c576d8b32391fc827ed43bac46245bc
alan <p9002>
2016-11-24 12:38:31 +0800

    Initial commit

---
+++ app/src/main/java/alan/no1/ys102401_1/RecActivity.java
@@ -0,0 +1,20 @@
+package alan.no1.ys102401_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rec);
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        Intent it = getIntent();
+        String data = it.getStringExtra(Intent.EXTRA_TEXT);
+        tv.setText(data);
+
+    }
+}
